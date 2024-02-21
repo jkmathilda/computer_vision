471c20702c80ab9076872c665440d69c32d10b1e
jkmathilda <jkmathilda627@gmail.com>
2024-02-20 23:01:21 -0800

    Upload contours.py

---
+++ 02_basics/170_contours.py
@@ -0,0 +1,39 @@
+import os
+import cv2
+
+# read image
+print("read image...")
+img = cv2.imread(os.path.join('.', 'data', 'contours_birds.jpg'))
+
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ret, thresh = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY_INV)
+
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+for cnt in contours:
+    if cv2.contourArea(cnt) > 200:
+        # cv2.drawContours(img, cnt, -1, (0, 255, 0), 1)
+
+        x1, y1, w, h = cv2.boundingRect(cnt)
+
+        cv2.rectangle(img, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
+
+# visualize image
+print("visualize image...")
+cv2.imshow('original', img)
+cv2.imshow('img_gray', img_gray)
+cv2.imshow('thresh', thresh)
+
+cv2.moveWindow('img_gray', 200,200)
+cv2.moveWindow('thresh', 400,400)
+
+# waiting
+print("### if you want exit, press a 'ESC' key ###")
+while(True):
+    key = cv2.waitKey(30)
+    if key == 27:   # ESC key
+        break
+
+cv2.destroyAllWindows()
+
